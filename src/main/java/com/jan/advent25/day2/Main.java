77a42378b487c582098718b93c148aa1f048e6d1
Jan Jungwirth <hrjungwirthjan@gmail.com>
2025-12-02 14:29:05 +0100

    Day 2 - Star 2

--- src/main/java/com/jan/advent25/day2/Main.java
+++ src/main/java/com/jan/advent25/day2/Main.java
@@ -1,23 +1,37 @@
 package com.jan.advent25.day2;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("expected true : " + RangeChecker.isValid(12345678));
-        System.out.println("expected true : " +RangeChecker.isValid(123));
-
-        System.out.println("expected flase : " +RangeChecker.isValid(123123));
-        System.out.println("expected flase : " +RangeChecker.isValid(11));
-        System.out.println("expected flase : " +RangeChecker.isValid(1212));
-        RangeLoader loader = new RangeLoader();
+        //System.out.println("expected true : " + RangeChecker.isValidExtended(12345678L));
+        //System.out.println("expected true : " +RangeChecker.isValidExtended(123L));
+        //System.out.println("expected flase : " +RangeChecker.isValidExtended(123123L));
+        //System.out.println("expected flase : " +RangeChecker.isValidExtended(11L));
+        //System.out.println("expected flase : " +RangeChecker.isValidExtended(1212L));
+        //System.out.println("expected flase : " +RangeChecker.isValidExtended(1111111L));
+        //System.out.println("expected flase : " +RangeChecker.isValidExtended(2121212121L));
+        RangeLoader loader = new RangeLoader("src/main/java/com/jan/advent25/day2/inputDay2.txt");
         RangeChecker checker = new RangeChecker();
 
         loader.getRanges().forEach(checker::checkRange);
         long result = 0;
+
+        checker.getCapturedInvalidIDs().remove(1L);
+        checker.getCapturedInvalidIDs().remove(2L);
+        checker.getCapturedInvalidIDs().remove(3L);
+        checker.getCapturedInvalidIDs().remove(4L);
+        checker.getCapturedInvalidIDs().remove(5L);
+        checker.getCapturedInvalidIDs().remove(6L);
+        checker.getCapturedInvalidIDs().remove(7L);
+        checker.getCapturedInvalidIDs().remove(8L);
+        checker.getCapturedInvalidIDs().remove(9L);
+
         for (Long capturedInvalidID : checker.getCapturedInvalidIDs()) {
-            result+=capturedInvalidID;
+            result += capturedInvalidID;
         }
+
+
+        System.out.println(checker.getCapturedInvalidIDs());
         System.out.println("result : " + result);
 
     }
--- src/main/java/com/jan/advent25/day2/RangeChecker.java
+++ src/main/java/com/jan/advent25/day2/RangeChecker.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class RangeChecker {
     @Getter
     List<Long> capturedInvalidIDs = new LinkedList<>();
+
     public static boolean isValid(long input){
         String length = String.valueOf(input);
         int inputLenght = length.length();
@@ -21,10 +22,55 @@ public class RangeChecker {
         return !firstHalf.equals(secondHalf);
     }
 
+    public static boolean isValidExtended(long input){
+        String check = String.valueOf(input);
+        int inputLength = check.length();
+
+        char[] checkBits = check.toCharArray();
+        char checkBit = checkBits[0];
+        boolean con = true;
+        for(char c : checkBits){
+            if (c != checkBit) {
+                con = false;
+                break;
+            }
+        }
+        if(con) return false;
+
+        //Case uneven in thirds eg. 999 : 1111111
+        if(inputLength%2!=0) {
+            if (checkInThirds(check)) {
+                return false;
+            }
+        }
+
+        if(inputLength%2==0) {
+            String firstHalf = check.substring(0, inputLength/2);
+            String secondHalf = check.substring(inputLength/2);
+            if(firstHalf.equals(secondHalf))
+                return false;
+            else{
+                if(check.length()>2)
+                    return !checkInThirds(check) && !(check.split(check.substring(0, 2)).length == 0);
+                else return true;
+            }
+        }
+        return true;
+    }
+
+    private static boolean checkInThirds(String length) {
+        int inputLenght = length.length();
+
+        String firstThird = length.substring(0, inputLenght / 3);
+        String secondThird = length.substring(inputLenght / 3, (inputLenght / 3) * 2);
+        String thirdThird = length.substring((inputLenght / 3) * 2);
+        return firstThird.equals(secondThird) && secondThird.equals(thirdThird);
+    }
+
     public List<Long> checkRange(Range range){
         List<Long> invalidIDs = new LinkedList<>();
         for(long i = range.getMin(); i<= range.getMax(); i++){
-            if(!isValid(i))
+            if(!isValidExtended(i))
                 invalidIDs.add(i);
         }
         this.capturedInvalidIDs.addAll(invalidIDs);
